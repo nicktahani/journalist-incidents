42b52a89b2356288ceb0be9f1e4c78751c0f015b
nick t <ns.tahani@gmail.com>
2022-07-04 21:38:41 -0700

    forgot to save file for commit

---
+++ src/util/map.js
@@ -0,0 +1,13 @@
+export function getCountsByYear(data) {
+  const counts = data.reduce((c, { year, country }) => {
+    if (!c[year]) c[year] = {}        
+    if (!c[year][country]) c[year][country] = {country, count: 0}
+    c[year][country].count++
+
+    return c
+  }, {})
+
+  const { undefined, ...filtered } = counts
+
+  return filtered
+}
