9a0962754bf348f04eb78db03bd68bd43f75cea2
nick t <ns.tahani@gmail.com>
2022-08-24 11:54:16 -0700

    modal updates/styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,8 +13,9 @@ export default function Modal({ data, isOpen, onClose, selected }) {
       <div className='modal'>
         <button onClick={onClose}>X</button>
         {data.map(d => (
-          <div>
-            {d.name}
+          <div key={d.id}>
+            <span><a href={d.page}>{d.name}</a></span>
+            <p>{d.body}</p>
           </div>
         ))}
       </div>
